6c31c808873e931a53138839b000cd710a236ecb
Monsieurmm <51748513+Monsieurmm@users.noreply.github.com>
2020-02-19 16:35:08 +0100

    feat: PATCH request for consultants
    may need code review / refactor

--- api/src/consultants/consultants.controller.ts
+++ api/src/consultants/consultants.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Body, Get, Post, Patch, Delete, Param } from '@nestjs/common';
 import { ConsultantsService } from './consultants.service';
+import { response } from 'express';
 
 @Controller('consultants')
 export class ConsultantsController {
@@ -35,9 +36,25 @@ export class ConsultantsController {
         return this.consultantsService.getConsultant(consulId);
     }
 
-    @Patch() // TODO
-    updateConsultant(): string {
-        return 'Consultant updated'
+    @Patch(':id') 
+    updateConsultant(
+        @Param('id') consulId: string,
+        @Body('first_name') first_name: string,
+        @Body('last_name') last_name: string,
+        @Body('starts_after_month') starts_after_month: number,
+        @Body('starts_after_years') starts_after_years: number,
+        @Body('leaves_after_month') leaves_after_month: number,
+        @Body('leaves_after_years') leaves_after_years: number
+    ) {
+        this.consultantsService.updateConsultant(
+            consulId,
+            first_name,
+            last_name,
+            starts_after_month,
+            starts_after_years,
+            leaves_after_month,
+            leaves_after_years
+        );
     }
 
     @Delete() // TODO
--- api/src/consultants/consultants.service.ts
+++ api/src/consultants/consultants.service.ts
@@ -10,7 +10,7 @@ export class ConsultantsService {
             starts_after_month: number,
             starts_after_years: number,
             leaves_after_month: number,
-            leaves_after_years: number
+            leaves_after_years: number,
         ) {
         const consulId = Date.now();
         const newConsultant = new Consultant(
@@ -20,7 +20,7 @@ export class ConsultantsService {
                 starts_after_month,
                 starts_after_years,
                 leaves_after_month,
-                leaves_after_years
+                leaves_after_years,
             );
         this.consultants.push(newConsultant);
         return consulId
@@ -34,6 +34,38 @@ export class ConsultantsService {
         const consultant = this.findConsultant(consultantId)[0];
         return { ...consultant };
     }
+
+    updateConsultant(
+            consultantId: string,
+            first_name: string,
+            last_name: string,
+            starts_after_month: number,
+            starts_after_years: number,
+            leaves_after_month: number,
+            leaves_after_years: number,
+        ) {
+        const [consultant, index] = this.findConsultant(consultantId);
+        const updateConsultant = { ...consultant };
+        if (last_name) {
+            updateConsultant.last_name = last_name;
+        }
+        if (first_name) {
+            updateConsultant.first_name = first_name;
+        }
+        if (starts_after_month) {
+            updateConsultant.starts_after_month = starts_after_month;
+        }
+        if (starts_after_years) {
+            updateConsultant.starts_after_years = starts_after_years;
+        }
+        if (leaves_after_month) {
+            updateConsultant.leaves_after_month = leaves_after_month;
+        }
+        if (leaves_after_years) {
+            updateConsultant.leaves_after_years = leaves_after_years;
+        }
+          this.consultants[index] = updateConsultant;
+        }
 
     private findConsultant(id: string): [Consultant, number] {
         const consultantIndex = this.consultants.findIndex(cons => cons.id === parseInt(id));
