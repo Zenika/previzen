b88d448efb22fe5854a4ef47d7e3f87851d70724
Anthony Pillot <pillot.anthony@gmail.com>
2020-02-20 17:47:28 +0100

    feat: just add Zenika logo and font-family

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,75 +1,98 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer app right v-model="drawer">
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
+    <v-app id="inspire">
+        <v-navigation-drawer app right v-model="drawer">
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-home</v-icon>
+                    </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
 
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-contact-mail</v-icon>
-          </v-list-item-action>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-contact-mail</v-icon>
+                    </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+                    <v-list-item-content>
+                        <v-list-item-title>Contact</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
 
-    <v-app-bar app color="brown" dark>
-      <v-spacer />
+        <v-app-bar app color="brown" dark>
 
-      <v-toolbar-title>{{application.name}}</v-toolbar-title>
+            <img alt="zenika_logo" id="zenika_logo" src="./assets/png/zenika_logo_white.png">
 
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-    </v-app-bar>
+            <v-spacer/>
 
-    <v-content>
-      <v-container>
-        <h1>{{ hello }}</h1>
-      </v-container>
-    </v-content>
+            <v-toolbar-title class="font-pacifico">{{application.name}}</v-toolbar-title>
 
-    <v-footer app color="brown">
-      <span class="white--text">&copy; {{application.corporation}}</span>
-      <v-spacer />
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+        </v-app-bar>
 
-      <span class="white--text">{{application.version}}</span>
-    </v-footer>
-  </v-app>
+        <v-content>
+            <v-container>
+                <h1>{{ hello }}</h1>
+
+                <!-- Center content should be here -->
+
+            </v-container>
+        </v-content>
+
+        <v-footer app color="brown">
+            <span class="white--text">&copy; {{application.corporation}}, {{application.year}}</span>
+            <v-spacer/>
+
+            <span class="white--text">{{application.version}}</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import axios from "axios";
-export default {
-  name: "LayoutsDemosBaselineFlipped",
+    import axios from "axios";
 
-  props: {
-    source: String
-  },
-  data: () => ({
-    drawer: null,
-    application: {
-      name: "PreviZen",
-      version: "ver. 0.0.1",
-      corporation: "Zenika"
-    },
-    hello: null
-  }),
-  mounted() {
-    axios
-        .get("http://localhost:3000")
-        .then(res => {
-            this.hello = res.data;
-    });
-  }
-};
+    export default {
+        name: "LayoutsDemosBaselineFlipped",
+
+        props: {
+            source: String
+        },
+        data: () => ({
+            drawer: null,
+            application: {
+                name: "PreviZen",
+                version: "ver. 0.0.1",
+                corporation: "Zenika",
+                year: '2020'
+            },
+            hello: null
+        }),
+        mounted() {
+            axios
+                .get("http://localhost:3000")
+                .then(res => {
+                    this.hello = res.data;
+                });
+        }
+    };
 </script>
+
+<style>
+
+    @import url('https://fonts.googleapis.com/css?family=Pacifico&display=swap');
+
+    .font-pacifico {
+        font-family: 'Pacifico', cursive;
+    }
+
+    #zenika_logo {
+        width: 70px;
+        padding-top: 40px;
+    }
+
+</style>
