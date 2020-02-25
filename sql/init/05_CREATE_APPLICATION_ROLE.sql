e7d18b7c87033c7868ba9caea176d289afe79bd0
Anthony Pillot <pillot.anthony@gmail.com>
2020-02-25 10:33:25 +0100

    feat: add application account in db with restrictive access

---
+++ sql/init/05_CREATE_APPLICATION_ROLE.sql
@@ -0,0 +1,31 @@
+-- Here is the 'application' account used by Nest.js API with restrictives access.
+
+-- Role: application
+-- DROP ROLE application;
+
+CREATE ROLE application WITH
+  LOGIN
+  NOSUPERUSER
+  NOINHERIT
+  NOCREATEDB
+  NOCREATEROLE
+  REPLICATION
+  PASSWORD ${PREVIZEN_PASSWORD};
+
+COMMENT ON ROLE application IS 'Application account used by Nest.js API with restrictives access.';
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.absence_jaz TO application;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.absence_off TO application;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.agency TO application;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.billable_day TO application;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.consultant TO application;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.customer TO application;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.daily_cost TO application;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.staffing TO application;
