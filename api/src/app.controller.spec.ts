862d10eec09dfc54702cdd76eb29bda479c37c32
Anthony Pillot <pillot.anthony@gmail.com>
2020-02-26 11:47:46 +0100

    refactor: rename root message and method

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "PreviZen Nest.js API is running"', () => {
+      expect(appController.getCheckHealthMessage()).toBe('PreviZen Nest.js API is running');
     });
   });
 });
