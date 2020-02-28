aa2c166aa3a294435cffaced656f53e64ed2f87c
Anthony Pillot <pillot.anthony@gmail.com>
2020-02-28 16:40:42 +0100

    refactor: let to const

--- api/src/agencies/agencies.service.ts
+++ api/src/agencies/agencies.service.ts
@@ -11,24 +11,23 @@ export class AgenciesService {
     ) {
     }
 
-    create(agency: Agency): Promise<Agency> {
-        let newAgency = new Agency;
+    findAll(): Promise<Agency[]> {
+        this.agenciesRepository.find().then(value => console.table(value));
+        return this.agenciesRepository.find();
+    }
 
+    findOne(id_agency: string): Promise<Agency> {
+        return this.agenciesRepository.findOne(id_agency);
+    }
+
+    create(agency: Agency): Promise<Agency> {
+        const newAgency = new Agency;
         newAgency.nameAgency = agency.nameAgency;
         newAgency.nameManager = agency.nameManager;
         newAgency.cityAgency = agency.cityAgency;
         newAgency.countryAgency = agency.countryAgency;
 
-        return this.agenciesRepository.save(newAgency)
-    }
-
-    findAll(): Promise<Agency[]> {
-        this.agenciesRepository.find().then(value => console.table(value));
-        return this.agenciesRepository.find()
-    }
-
-    findOne(id_agency: string): Promise<Agency> {
-        return this.agenciesRepository.findOne(id_agency);
+        return this.agenciesRepository.save(newAgency);
     }
 
     async remove(id_agency: string): Promise<void> {
