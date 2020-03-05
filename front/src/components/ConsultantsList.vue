1c4bfe45c85f388c532efd279108757cf32f63cf
monsieurmm <mniepceron@gmail.com>
2020-03-05 17:02:55 +0100

    fix: correct minor format

--- front/src/components/ConsultantsList.vue
+++ front/src/components/ConsultantsList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout row wrap>
-    <!-- <v-flex xs6> TODO
+    <!-- <v-flex xs6>
       <v-select
         v-model="filter"
         :items="agencies"
@@ -8,7 +8,7 @@
         item-value="idAgency"
         label="Filter by agency"
       ></v-select>
-    </v-flex>-->
+    </v-flex> -->
 
     <v-flex xs6>
       <v-text-field
@@ -35,7 +35,14 @@
                       <v-text-field v-model="editedItem.firstNameConsultant" label="First Name"></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="idAgency" label="Agency"></v-text-field>
+                      <v-select
+                        v-model="editedItem.idAgency"
+                        :items="agencies"
+                        item-text="nameAgency"
+                        item-value="idAgency"
+                        label="Agency"
+                        required
+                      ></v-select>
                     </v-col>
                     <v-col cols="12" sm="6" md="3">
                       <v-text-field
@@ -64,13 +71,13 @@
               <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn color="blue darken-1" text @click="dialog = false">Cancel</v-btn>
-                <v-btn color="blue darken-1" text>Edit</v-btn> <!-- TODO @click -->
+                <v-btn color="blue darken-1" text>Edit</v-btn> <!-- TODO save the updated data -->
               </v-card-actions>
             </v-card>
           </v-dialog>
         </template>
         <template v-slot:item.action="{ item }">
-          <v-icon small class="mr-2">mdi-cogs</v-icon> <!-- TODO @click -->
+          <v-icon small class="mr-2" @click="editConsultant(item)">mdi-cogs</v-icon>
           <v-icon small @click="deleteConsultant(item.idConsultant)">mdi-delete</v-icon>
         </template>
       </v-data-table>
@@ -90,8 +97,8 @@ export default {
       dialog: false,
       agencies: [],
       consultants: [],
-      editedItem: -1,
-      editedIndex: {
+      editedIndex: -1,
+      editedItem: {
         lastNameConsultant: "",
         firstNameConsultant: "",
         startsAfterMonthConsultant: 0,
@@ -139,19 +146,11 @@ export default {
         .get("http://localhost:3000/agencies")
         .then(response => (this.agencies = response.data));
     },
-    // editConsultant(item) { TODO
-    //   this.editedIndex = this.consultants.indexOf(item);
-    //   this.editedItem = Object.assign({}, item);
-    //   this.dialog = true;
-    // },
-    // save() {
-    //   if (this.editedIndex > -1) {
-    //     Object.assign(this.consultants[this.editedIndex], this.editedItem);
-    //   } else {
-    //     this.consultants.push(this.editedItem);
-    //   }
-    //   this.dialog = false;
-    // },
+    editConsultant(item) {
+      this.editedIndex = this.consultants.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
     deleteConsultant(id) {
       const index = this.consultants.indexOf(id);
       confirm("Are you sure you want to delete this consultant?") &&
--- front/src/components/addAgency.vue
+++ front/src/components/addAgency.vue
@@ -1,7 +1,9 @@
 <template>
   <v-dialog>
     <template v-slot:activator="{ on }">
-      <v-btn v-on="on" text depressed>Add new agency</v-btn>
+      <v-btn v-on="on" text depressed>Add new agency
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
     </template>
     <v-card>
       <v-card-title>Add a new agency</v-card-title>
@@ -33,7 +35,7 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   name: "addAgency",
   data() {
@@ -52,13 +54,15 @@ export default {
         nameManager: this.nameManager,
         cityAgency: this.cityAgency,
         countryAgency: this.countryAgency
-      }
+      };
       console.table(newAgency);
 
       axios
-        .post('http://localhost:3000/agencies', newAgency)
-        .then(response => (response.data))
-        .catch(error => {throw error})
+        .post("http://localhost:3000/agencies", newAgency)
+        .then(response => response.data)
+        .catch(error => {
+          throw error;
+        });
     }
   }
 };
