f685157314b2798231afecaa8e6ca4aebc8f9767
Anthony Pillot <pillot.anthony@gmail.com>
2020-03-05 11:32:59 +0100

    test: first tests in consultants.controller.spec.ts

--- api/src/consultants/consultants.controller.spec.ts
+++ api/src/consultants/consultants.controller.spec.ts
@@ -1,18 +1,39 @@
+import { ConsultantsService } from './consultants.service';
+import { Repository } from 'typeorm';
+import { Consultant } from './consultant.entity';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ConsultantsController } from './consultants.controller';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('Consultants Controller', () => {
-  let controller: ConsultantsController;
+  let consultantsService: ConsultantsService;
+  let consultantRepository: Repository<Consultant>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [ConsultantsController],
+      providers: [
+        ConsultantsService,
+        {
+          provide: getRepositoryToken(Consultant),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<ConsultantsController>(ConsultantsController);
+    consultantsService = module.get<ConsultantsService>(ConsultantsService);
+    consultantRepository = module.get<Repository<Consultant>>(
+      getRepositoryToken(Consultant),
+    );
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return an array of Consultant', async () => {
+    const consultantTest = new Consultant();
+    const result = Promise.resolve([consultantTest]);
+    jest.spyOn(consultantsService, 'findAll').mockImplementation(() => result);
+  });
+
+  it('should return a Consultant', async () => {
+    const consultantTest = new Consultant();
+    const result = Promise.resolve(consultantTest);
+    jest.spyOn(consultantsService, 'findOne').mockImplementation(() => result);
   });
 });
