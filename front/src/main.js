a95e09ce80b7516fd281fe47a7b668b61774fe60
monsieurmm <mniepceron@gmail.com>
2020-03-05 16:10:39 +0100

    docs: deleted plugin import

--- front/src/main.js
+++ front/src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios';
 import vuetify from './plugins/vuetify';
-import MultiFiltersPlugin from './plugins/MultiFilters'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$http = axios
 
-Vue.use(MultiFiltersPlugin);
 
 
 new Vue({
