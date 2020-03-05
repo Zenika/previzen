52ad0b0dc35e46f162c29251ecea0ccc9808f3a9
monsieurmm <mniepceron@gmail.com>
2020-03-05 16:11:56 +0100

    feat: create consultant form

--- front/src/components/addConsultant.vue
+++ front/src/components/addConsultant.vue
@@ -91,14 +91,11 @@
             </v-menu>
           </v-col>
         </v-row>
-        <v-row>
-          <v-col cols="12" xs="6" sm="6" md="2">
-            <v-btn @click="createConsultant()" color="success">Create</v-btn>
-          </v-col>
-          <v-col cols="12" xs="6" sm="6" md="2">
-            <v-btn @click="dialog = false" color="red">Cancel</v-btn>
-          </v-col>
-        </v-row>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Cancel</v-btn>
+          <v-btn color="blue darken-1" text @click="createConsultant()">Create</v-btn>
+        </v-card-actions>
       </v-form>
     </v-card>
   </v-dialog>
@@ -112,7 +109,7 @@ export default {
     return {
       lastNameConsultant: "",
       firstNameConsultant: "",
-      idAgency: null,
+      idAgency: '',
       agencies: [],
       startDate: "",
       endDate: "",
@@ -122,12 +119,7 @@ export default {
       dateRules: [v => !!v || "Date is required !"],
       dialog: false,
       menu: false,
-      menu2: false,
-      editedConsultant: -1,
-      editedIndex: {
-        lastNameConsultant: "",
-        firstNameConsultant: ""
-      }
+      menu2: false
     };
   },
   methods: {
@@ -142,7 +134,7 @@ export default {
       let newConsultant = {
         lastNameConsultant: this.lastNameConsultant.toUpperCase(),
         firstNameConsultant: this.firstNameConsultant,
-        idAgency: this.idAgency,
+        idAgency: parseInt(this.idAgency),
         startsAfterMonthConsultant: parseInt(arrayStartDate[1]),
         startsAfterYearConsultant: parseInt(arrayStartDate[0]),
         leavesAfterMonthConsultant: parseInt(arrayEndDate[1]),
