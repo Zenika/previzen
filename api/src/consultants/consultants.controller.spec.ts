d33dbc4ad4fdd90050c5a2ae30d96f86faa089fe
Anthony Pillot <pillot.anthony@gmail.com>
2020-03-05 11:51:27 +0100

    test: new test with spyOn(), but WIP

--- api/src/consultants/consultants.controller.spec.ts
+++ api/src/consultants/consultants.controller.spec.ts
@@ -3,8 +3,10 @@ import { Repository } from 'typeorm';
 import { Consultant } from './consultant.entity';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { ConsultantsController } from './consultants.controller';
 
 describe('Consultants Controller', () => {
+  let consultantsController: ConsultantsController;
   let consultantsService: ConsultantsService;
   let consultantRepository: Repository<Consultant>;
 
@@ -17,23 +19,40 @@ describe('Consultants Controller', () => {
           useClass: Repository,
         },
       ],
+      controllers: [ConsultantsController],
     }).compile();
 
+    consultantsController = module.get(ConsultantsController);
     consultantsService = module.get<ConsultantsService>(ConsultantsService);
     consultantRepository = module.get<Repository<Consultant>>(
       getRepositoryToken(Consultant),
     );
   });
 
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should return an entity of Consultant', async () => {
+    const consultantTest = new Consultant();
+    const result = Promise.resolve(consultantTest);
+    jest.spyOn(consultantsService, 'findOne').mockImplementation(() => result);
+  });
+
+  it('should return an entity of Consultant', async () => {
+    const expectedResult = new Consultant();
+    const mockNumberToSatisfyParameters = '0';
+
+    jest.spyOn(consultantsService, 'findOne').mockResolvedValue(expectedResult);
+
+    expect(
+      await consultantsController.findOne(mockNumberToSatisfyParameters),
+    ).toBe(expectedResult);
+  });
+
   it('should return an array of Consultant', async () => {
     const consultantTest = new Consultant();
     const result = Promise.resolve([consultantTest]);
     jest.spyOn(consultantsService, 'findAll').mockImplementation(() => result);
-  });
-
-  it('should return a Consultant', async () => {
-    const consultantTest = new Consultant();
-    const result = Promise.resolve(consultantTest);
-    jest.spyOn(consultantsService, 'findOne').mockImplementation(() => result);
   });
 });
