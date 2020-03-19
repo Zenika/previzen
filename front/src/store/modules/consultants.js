71853420c6c1895b839a3b67109e3eea549fb831
monsieurmm <mniepceron@gmail.com>
2020-03-19 15:25:16 +0100

    feat: import consultants management pattern from store/index.js

---
+++ front/src/store/modules/consultants.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+const consultants = {
+  namespaced: true,
+
+  state: {
+    consultants: []
+  },
+
+  mutations: {
+    SET_CONSULTANTS: (state, consultant) => {
+      state.consultants = consultant;
+    },
+    ADD_CONSULTANT: (state, consultant) => {
+      state.consultants.push(consultant);
+    },
+    REMOVE_CONSULTANT: (state, id) => {
+      const index = state.consultants
+      .map(consultants => consultants.idConsultant)
+      .indexOf(id);
+      state.consultants.splice(index, 1);
+    }
+  },
+
+  actions: {
+    GET_CONSULTANTS: async ({ commit }) => {
+      await axios
+        .get("http://localhost:3000/consultants")
+        .then(response => {
+          let consultants = response.data;
+          commit("SET_CONSULTANTS", consultants);
+        })
+        .catch(error => {
+          throw error;
+        });
+    },
+    CREATE_CONSULTANT: async ({ commit }, consultant) => {
+      const response = await axios
+        .post("http://localhost:3000/consultants", consultant)
+        commit("ADD_CONSULTANT", response.data);
+    },
+    DELETE_CONSULTANT: async ({ commit }, id) => {
+      await axios
+        .delete(`http://localhost:3000/consultants/${id}`)
+      commit("REMOVE_CONSULTANT", id)
+    }
+  }
+}
+
+export default consultants;
