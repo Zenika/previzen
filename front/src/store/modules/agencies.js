f183aea785933f0fee8faa435688e54605c8b2f0
monsieurmm <mniepceron@gmail.com>
2020-03-20 16:27:11 +0100

    feat: state management pattern for agencies

--- front/src/store/modules/agencies.js
+++ front/src/store/modules/agencies.js
@@ -9,6 +9,18 @@ const agencies = {
   mutations: {
     SET_AGENCIES: (state, agency) => {
       state.agencies = agency
+    },
+    ADD_AGENCY: (state, agency) => {
+      state.agencies.push(agency);
+    },
+    EDIT_AGENCY: (state, item) => {
+      state.agencies.item = item
+    },
+    REMOVE_AGENCY: (state, id) => {
+      const index = state.agencies
+      .map(agencies => agencies.idAgency)
+      .indexOf(id);
+      state.agencies.splice(index, 1);
     }
   },
 
@@ -23,6 +35,22 @@ const agencies = {
         .catch(error => {
           throw error
         })
+    },
+    CREATE_AGENCY: async ({ commit }, agency) => {
+      const response = await axios
+        .post("http://localhost:3000/agencies", agency)
+        commit("ADD_AGENCY", response.data);
+    },
+    DELETE_AGENCY: async ({ commit }, id) => {
+      await axios
+        .delete(`http://localhost:3000/agencies/${id}`)
+      commit("REMOVE_AGENCY", id)
+    },
+    UPDATE_AGENCY: async ({ commit }, item) => {
+      const response = await axios
+        .put(`http://localhost:3000/agencies/${item.idAgency}`, item)
+        .then(console.log(item))
+        commit("EDIT_AGENCY", response.data)
     }
   }
 }
