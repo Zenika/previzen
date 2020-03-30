12675ccb7c005cea7ebc51d9548fc52ffc9288b8
monsieurmm <mniepceron@gmail.com>
2020-03-30 15:04:56 +0200

    chore: should correct conflict

--- front/src/store/modules/consultants.js
+++ front/src/store/modules/consultants.js
@@ -13,15 +13,15 @@ const consultants = {
     ADD_CONSULTANT: (state, consultant) => {
       state.consultants.push(consultant);
     },
+    REMOVE_CONSULTANT: (state, id) => {
+      const index = state.consultants
+        .map(consultants => consultants.idConsultant)
+        .indexOf(id);
+      state.consultants.splice(index, 1);
+    },
     EDIT_CONSULTANT: (state, payload) => {
       state.consultants = payload
     },
-    REMOVE_CONSULTANT: (state, id) => {
-      const index = state.consultants
-      .map(consultants => consultants.idConsultant)
-      .indexOf(id);
-      state.consultants.splice(index, 1);
-    }
   },
 
   actions: {
@@ -39,23 +39,23 @@ const consultants = {
     CREATE_CONSULTANT: async ({ commit }, consultant) => {
       let response = await axios
         .post("http://localhost:3000/consultants", consultant)
-        if(response.status == 201) {
-          commit("ADD_CONSULTANT", response.data);
-        }
-    },
-    UPDATE_CONSULTANT: async ({ commit }, id) => {
-      let response = await axios
-        .put(`http://localhost:3000/consultants/${id}`);
-        commit("EDIT_CONSULTANT", response.data);
-        console.log(response.data);
+      if (response.status == 201) {
+        commit("ADD_CONSULTANT", response.data);
+      }
     },
     DELETE_CONSULTANT: async ({ commit }, id) => {
       let response = await axios
         .delete(`http://localhost:3000/consultants/${id}`)
-        if(response.status == 200 || response.status == 204) {
-          commit("REMOVE_CONSULTANT", id);
-        }
-    }
+      if (response.status == 200 || response.status == 204) {
+        commit("REMOVE_CONSULTANT", id);
+      }
+    },
+    UPDATE_CONSULTANT: async ({ commit }, id) => {
+      let response = await axios
+        .put(`http://localhost:3000/consultants/${id}`);
+      commit("EDIT_CONSULTANT", response.data);
+      console.log(response.data);
+    },
   }
 }
 
