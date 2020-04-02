a6254b807421cb951940da4d10c940afc437d367
monsieurmm <mniepceron@gmail.com>
2020-04-02 10:56:39 +0200

    feat: staffing service now return the data from consultant's and customer's table

--- api/src/staffing/staffing.service.ts
+++ api/src/staffing/staffing.service.ts
@@ -8,21 +8,30 @@ export class StaffingService {
   constructor(
     @InjectRepository(Staffing)
     private readonly staffingRepository: Repository<Staffing>,
-  ) {}
+  ) { }
 
   findAll(): Promise<Staffing[]> {
     return this.staffingRepository.find({
       join: {
-        alias: 'customer',
+        alias: 'staffing',
         leftJoinAndSelect: {
-          id_customer: 'customer.idCustomer'
+          consultant: "staffing.idConsultant",
+          customer: "staffing.idCustomer"
         },
       },
     });
   }
 
   findOne(id_staffing: string): Promise<Staffing> {
-    return this.staffingRepository.findOne(id_staffing);
+    return this.staffingRepository.findOne(id_staffing, {
+      join: {
+        alias: 'staffing',
+        leftJoinAndSelect: {
+          consultant: "staffing.idConsultant",
+          customer: "staffing.idCustomer"
+        },
+      },
+    });
   }
 
   create(staffing: Staffing): Promise<Staffing> {
